681cca0e73dabf5a96f9d03128fe51cd707f1101
Matthew Paletta <mattpaletta@gmail.com>
2018-12-31 18:14:52 -0800

    Initial commit

---
+++ src/BigFloat.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Matthew Paletta on 2018-12-31.
+//
+
+#include <iostream>
+#include "BigFloat.h"
+
+BigFloat::BigFloat() {
+    std::cout << "Creating Big Float" << std::endl;
+}
+
+BigFloat::~BigFloat() {
+    std::cout << "Deleting Big Float" << std::endl;
+
+}
